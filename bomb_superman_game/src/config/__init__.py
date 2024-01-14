c2f84bd513b74f8d52ab292fc611ebf20dab8454
peter921123 <peter921123@gmail.com>
2024-01-14 18:12:42 +0800

    Fixed window_background bug in main.py and config, and add window_background pic.

--- bomb_superman_game/src/config/__init__.py
+++ bomb_superman_game/src/config/__init__.py
@@ -1,3 +1,13 @@
 #
 # @file __init__.py
 #
+
+import pygame
+import os
+
+background_path = os.path.join(os.path.dirname(__file__), "..\..\img\\background\\background.png") # 背景圖片路徑
+
+window_length = 800 # 視窗長度
+window_width = 600 # 視窗寬度
+window_size = (window_length, window_width) # 視窗大小
+window_caption = "Bomb Superman" # 視窗標題
--- bomb_superman_game/src/main.py
+++ bomb_superman_game/src/main.py
@@ -1,23 +1,21 @@
 import sys
-
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import *
 
-# 初始化
-pygame.init()
-# 建立 window 視窗畫布，大小為 800x600
-window_surface = pygame.display.set_mode((800, 600))
-# 設置視窗標題為 Hello World:)
-pygame.display.set_caption('Hello World:)')
-# 清除畫面並填滿背景色
-window_surface.fill((255, 255, 255))
+from character import character
+from attribute import attribute
+from config import *
 
-# 宣告 font 文字物件
-head_font = pygame.font.SysFont(None, 60)
-# 渲染方法會回傳 surface 物件
-text_surface = head_font.render('Hello World!', True, (0, 0, 0))
-# blit 用來把其他元素渲染到另外一個 surface 上，這邊是 window 視窗
-window_surface.blit(text_surface, (10, 10))
+pygame.init() # 初始化pygame
+window_surface = pygame.display.set_mode(window_size) # 設定視窗大小
+pygame.display.set_caption(window_caption) # 設定視窗標題
+window_background = pygame.image.load(background_path).convert_alpha() # 載入背景圖片
+window_background = pygame.transform.scale(window_background, window_size) # 調整背景圖片大小
+window_surface.blit(window_background, (0, 0)) # 貼上背景圖片
+
+character = character.Character() # 建立角色物件
+character.set_pos(100, 100) # 設定角色位置
+window_surface.blit(character.image, character.rect) # 繪製角色
 
 # 更新畫面，等所有操作完成後一次更新（若沒更新，則元素不會出現）
 pygame.display.update()
@@ -30,3 +28,25 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_UP or event.key == K_w:
+                print('UP')
+                character.move_up()
+            elif event.key == K_DOWN or event.key == K_s:
+                print('DOWN')
+                character.move_down()
+            elif event.key == K_LEFT or event.key == K_a:
+                print('LEFT')
+                character.move_left()
+            elif event.key == K_RIGHT or event.key == K_d:
+                print('RIGHT')
+
+            elif event.key == K_SPACE:
+                print('SPACE')
+            elif event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        window_surface.blit(window_background, (0, 0))
+        window_surface.blit(character.image, character.rect)
+        pygame.display.update() # 更新畫面
