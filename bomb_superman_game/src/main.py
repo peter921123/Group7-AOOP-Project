20a4fbe2a9b54020c379598944fe98e3012b03ff
peter921123 <peter921123@gmail.com>
2024-01-15 19:14:44 +0800

    1. Add class bomb and its basic function. 2. Add images of bomb class. 3. Add clock to control fps and add update function of bomb.

--- bomb_superman_game/src/main.py
+++ bomb_superman_game/src/main.py
@@ -4,9 +4,13 @@ from pygame.locals import *
 
 from character import character
 from attribute import attribute
+from weapons import bomb
 from config import *
 
 pygame.init() # 初始化pygame
+
+clock = pygame.time.Clock() # 建立遊戲時鐘
+
 window_surface = pygame.display.set_mode(window_size) # 設定視窗大小
 pygame.display.set_caption(window_caption) # 設定視窗標題
 window_background = pygame.image.load(background_path).convert_alpha() # 載入背景圖片
@@ -17,13 +21,18 @@ character = character.Character() # 建立角色物件
 character.set_pos(100, 100) # 設定角色位置
 window_surface.blit(character.image, character.rect) # 繪製角色
 
-# 更新畫面，等所有操作完成後一次更新（若沒更新，則元素不會出現）
-pygame.display.update()
+bomb.Bomb.load_images() # 載入炸彈圖片
 
-# 事件迴圈監聽事件，進行事件處理
-while True:
-    # 迭代整個事件迴圈，若有符合事件則對應處理
-    for event in pygame.event.get():
+pygame.display.update() # 更新畫面
+
+pygame.event.set_blocked(pygame.MOUSEMOTION) # 鎖定滑鼠游標
+
+while True: # 事件迴圈監聽事件，進行事件處理
+
+    clock.tick(60) # 遊戲迴圈每秒執行60次
+
+    for event in pygame.event.get(): # 迭代整個事件迴圈，若有符合事件則對應處理
+
         # 當使用者結束視窗，程式也結束
         if event.type == QUIT:
             pygame.quit()
@@ -31,6 +40,7 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_SPACE:
                 print('SPACE')
+                bomb.Bomb(character.get_pos()[0], character.get_pos()[1])
             elif event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
@@ -51,6 +61,8 @@ while True:
 
         character.check_position()
 
-        window_surface.blit(window_background, (0, 0))
-        window_surface.blit(character.image, character.rect)
-        pygame.display.update() # 更新畫面
+    window_surface.blit(window_background, (0, 0))
+    bomb.Bomb.all_bombs.update() # 更新炸彈圖片
+    bomb.Bomb.all_bombs.draw(window_surface) # 繪製炸彈
+    window_surface.blit(character.image, character.rect)
+    pygame.display.update() # 更新畫面
---
+++ bomb_superman_game/src/weapons/bomb.py
@@ -0,0 +1,28 @@
+import os
+image_path = [os.path.join(os.path.dirname(__file__), f"..\..\img\weapons\\bomb_{i}.png") for i in range(4)]
+import pygame
+
+class Bomb(pygame.sprite.Sprite):
+
+    all_bombs = pygame.sprite.Group()
+    images_list = []
+
+    @staticmethod
+    def load_images():
+        Bomb.images_list = [pygame.image.load(i).convert_alpha() for i in image_path] # 載入圖片
+        Bomb.images_list = [pygame.transform.scale(i, (50, 50)) for i in Bomb.images_list] # 調整圖片大小
+
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 50)) # 建立一個 surface
+        self.rect = self.image.get_rect() # 取得圖片矩形
+        self.current_image = 0 # 目前圖片索引
+        self.image = Bomb.images_list[self.current_image] # 載入圖片
+        self.rect.x, self.rect.y = pos_x, pos_y # 設定圖片矩形位置
+        Bomb.all_bombs.add(self)
+
+    def update(self):
+        self.current_image += 0.25
+        if self.current_image >= len(Bomb.images_list):
+            self.current_image = 0
+        self.image = Bomb.images_list[int(self.current_image)] # 載入圖片
