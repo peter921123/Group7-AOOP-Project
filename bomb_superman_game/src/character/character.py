d35df1eee28e17d71af0ed7239bd95a704c75879
peter921123 <peter921123@gmail.com>
2024-01-17 20:40:36 +0800

    Remove old code for checking Character's position

--- bomb_superman_game/src/character/character.py
+++ bomb_superman_game/src/character/character.py
@@ -36,21 +36,36 @@ class Character(attribute.Attribute, mysprite.MySprite):
         if self.rect.y > window_size[1] - self.rect.height:
             self.rect.y = window_size[1] - self.rect.height
 
+    def move(self, direction):
+        direction_dict = {
+            "up": [0, -1],
+            "down": [0, 1],
+            "left": [-1, 0],
+            "right": [1, 0]
+        }
+        if len(pygame.sprite.spritecollide(self, mysprite.MySprite.all_sprites, False)) > 1:
+            self.rect.center = (self.rect.center[0] + direction_dict[direction][0] * self.get_speed(), self.rect.center[1] + direction_dict[direction][1] * self.get_speed())
+            return
+        # 如果在移動前已經與某個 sprite 碰撞，則可移動。
+
+        old_center = self.rect.center
+        self.rect.center = (self.rect.center[0] + direction_dict[direction][0] * self.get_speed(), self.rect.center[1] + direction_dict[direction][1] * self.get_speed())
+        if len(pygame.sprite.spritecollide(self, mysprite.MySprite.all_sprites, False)) > 1:
+            print("Character detect collision, can't move.")
+            self.rect.center = old_center
+        # 若移動後才碰撞到某個 sprite，則不移動。
+
     def move_up(self):
-        self.rect.y -= self.get_speed()
-        self.set_pos(self.rect.x, self.rect.y)
+        self.move("up")
 
     def move_down(self):
-        self.rect.y += self.get_speed()
-        self.set_pos(self.rect.x, self.rect.y)
+        self.move("down")
 
     def move_left(self):
-        self.rect.x -= self.get_speed()
-        self.set_pos(self.rect.x, self.rect.y)
+        self.move("left")
 
     def move_right(self):
-        self.rect.x += self.get_speed()
-        self.set_pos(self.rect.x, self.rect.y)
+        self.move("right")
 
     def place_bomb(self):
         if (self.get_current_bomb_number() < self.get_max_bomb_number()):
