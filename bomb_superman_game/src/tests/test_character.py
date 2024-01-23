aa5e7a0c28992dbc477b98a75a58469c1c0f4683
Peter <73233515+peter921123@users.noreply.github.com>
2024-01-24 00:56:32 +0800

    Delete bomb_superman_game/src/tests directory (old test code)

--- bomb_superman_game/src/tests/test_character.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from character import character
-
-def test_character():
-    test = character.Character()
-    
-    assert test.get_strength() == 1
-    assert test.get_speed() == 10
-    assert test.get_pos() == (0, 0)
